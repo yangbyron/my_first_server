ce9c85c7c5b13516956cbfac63723ab9c84df9c4
Bolun Yang <yangbyron998@gmail.com>
2022-09-22 11:28:32 -0500

    server exercise with routes in separate file

---
+++ routes.js
@@ -0,0 +1,11 @@
+routes = {
+    '/special-message': function(req, res) {
+      res.end("You're SPECIAL");
+    },
+  
+    '/non-special-message': function(req, res) {
+      res.end("You're boring!");
+    }
+  };
+  
+  module.exports = routes;
